798d49026099746d8e771d555b9405cb5587cd12
Piasy Xu <xz4215@gmail.com>
2018-09-02 09:58:13 +0800

    update to #24275

--- AppRTCMobile/ARDExternalSampleCapturer.h
+++ AppRTCMobile/ARDExternalSampleCapturer.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2014 The WebRTC Project Authors. All rights reserved.
+ *  Copyright 2018 The WebRTC Project Authors. All rights reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -8,10 +8,11 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
-#import "WebRTC/RTCMediaConstraints.h"
+#import <WebRTC/RTCVideoCapturer.h>
 
-@interface RTCMediaConstraints (JSON)
+@protocol ARDExternalSampleDelegate <NSObject>
+- (void)didCaptureSampleBuffer:(CMSampleBufferRef)sampleBuffer;
+@end
 
-+ (RTCMediaConstraints *)constraintsFromJSONDictionary:(NSDictionary *)dictionary;
-
+@interface ARDExternalSampleCapturer : RTCVideoCapturer <ARDExternalSampleDelegate>
 @end
